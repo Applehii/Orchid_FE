f1c407cfc6a0b80140bfa034ba8f473397b3332b
iamnotadev <qlv2810@gmail.com>
2025-06-30 21:42:22 +0700

    update_refresh_token

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -3,19 +3,15 @@ import { setToken, removeToken, getToken } from "./localStorageService";
 
 // Login
 export const login = async (email: string, password: string): Promise<any> => {
-  const response = await axios.post(
-    "http://localhost:8080/auth/signin",
-    {
-      email,
-      password,
-    }
-  );
+  const response = await axios.post("http://localhost:8080/auth/signin", {
+    email,
+    password,
+  });
 
-  const token  = response.data;
+  const token = response.data;
   setToken(token);
   return response.data.result; // trả về thông tin user
 };
-
 
 // Logout – xóa token ở localStorage
 export const logOut = () => {
@@ -23,37 +19,36 @@ export const logOut = () => {
   window.location.href = "/login"; // Chuyển về login nếu cần
 };
 
-
 export interface UserCreationRequest {
-    accountName: string;
-    email: string;
-    password: string;
-  }
+  accountName: string;
+  email: string;
+  password: string;
+}
 
 export const registerUser = async (user: UserCreationRequest): Promise<any> => {
-  const response = await axios.post(
-    "http://localhost:8080/auth/signup",
-    user
-  );
+  const response = await axios.post("http://localhost:8080/auth/signup", user);
   return response.data.result;
 };
 export const refreshAccessToken = async (): Promise<string> => {
   const currentToken = getToken();
-
+  if (!currentToken) throw new Error("No token found");
+  // Gửi token cũ dưới dạng raw string (text/plain)
   const response = await axios.post(
-    "http://localhost:8080/auth/refresh",
+    "http://localhost:8080/refresh",
+    currentToken,
     {
-      token: currentToken,
+      headers: {
+        "Content-Type": "text/plain",
+      },
     }
   );
-
-  const { token } = response.data;
-  return token;
+  return response.data;
 };
 
 // Lấy danh sách order của user hiện tại
 export const getMyOrders = async (): Promise<any[]> => {
-  const response = await axios.get("http://localhost:8080/orders/accounts/me/orders");
+  const response = await axios.get(
+    "http://localhost:8080/orders/accounts/me/orders"
+  );
   return response.data;
 };
-
